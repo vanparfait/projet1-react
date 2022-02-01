5f66dfe6a0bd233989823e363b9c721ccd166c18
vanparfait <nguemechiaparfait@gmail.com>
2022-02-01 07:25:00 +0100

    use state

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import Banner from './Banner'
+import logo from '../assets/logo.png'
 import Cart from './Cart'
+import Footer from './Footer'
 import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
 
 function App() {
 	return (
 		<div>
-			<Banner />
-			<Cart />
-			<ShoppingList />
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
+			<Footer />
 		</div>
 	)
 }
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,51 +1,87 @@
+import monstera from '../assets/monstera.jpg'
+import lyrata from '../assets/lyrata.jpg'
+import pothos from '../assets/pothos.jpg'
+import succulent from '../assets/succulent.jpg'
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
+
 export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
-		id: '1ed'
+		id: '1ed',
+		light: 2,
+		water: 3,
+		cover: monstera
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
-		id: '2ab'
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: lyrata
 	},
+
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-		isSpecialOffer: true
+		light: 1,
+		water: 2,
+		cover: pothos
 	},
 	{
-		name: 'yucca',
+		name: 'calathea',
 		category: 'classique',
-		id: '4kk'
+		id: '4kk',
+		light: 2,
+		water: 3,
+		cover: calathea
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-		isAvailable: false
+		light: 3,
+		water: 1,
+		cover: olivier
 	},
+
 	{
-		name: 'géranium',
-		category: 'extérieur',
-		id: '6uo'
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: cactus
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isSpecialOffer: true
-	},
-	{
-		name: 'aloe',
-		category: 'plante grasse',
-		id: '8fp',
-		isSpecialOffer: true
+		light: 2,
+		water: 3,
+		cover: basil
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn'
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulent
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint
 	}
 ]
